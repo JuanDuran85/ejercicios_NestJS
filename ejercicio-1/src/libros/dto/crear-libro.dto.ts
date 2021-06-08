eb397dc01497b4b5c307db310cf2735bef3db2a7
JuanDuran85 <duran85juan@gmail.com>
2021-06-07 22:52:49 -0400

    agregando validaciones con libraria externa

--- ejercicio-1/src/libros/dto/crear-libro.dto.ts
+++ ejercicio-1/src/libros/dto/crear-libro.dto.ts
@@ -1,7 +1,21 @@
 /* eslint-disable prettier/prettier */
 
+import { IsNotEmpty, IsNumber, IsPositive, IsString, Length, Max, Min } from 'class-validator';
+
 export class CrearLibroDto {
+    @IsString()
+    @IsNotEmpty()
+    @Length(2,20)
     nombre: string;
+
+    @IsString()
+    @IsNotEmpty()
     autor: string;
+    
+    @IsNumber()
+    @IsNotEmpty()
+    @IsPositive()
+    @Min(1)
+    @Max(2000)
     paginas: number;
 };
