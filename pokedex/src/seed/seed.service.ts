f78efd6a2eec90ebf89254593b5a5a6a00803c5c
JuanDuran85 <duran85juan@gmail.com>
2025-12-19 11:37:03 -0300

    Add seed module with PokeAPI integration
    - Add axios dependency for HTTP requests
    - Create seed module with controller and service
    - Implement PokeAPI interface for pokemon data fetching
    - Add seed endpoint to populate database with pokemon data
    - Register SeedModule in main application module

---
+++ pokedex/src/seed/seed.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+  public async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const no: number = Number(url.split('/').at(-2));
+      console.debug({ name, no });
+    });
+    return data;
+  }
+}
