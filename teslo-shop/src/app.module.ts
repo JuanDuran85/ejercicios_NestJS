e30fbd31a1ee7c1e14ed95c197a29ea8ea07ab9e
JuanDuran85 <duran85juan@gmail.com>
2025-12-21 00:01:58 -0300

    feat: add products module with basic CRUD operations

--- teslo-shop/src/app.module.ts
+++ teslo-shop/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: process.env.DB_SYNC === 'true',
       logging: true,
     }),
+    ProductsModule,
   ],
   controllers: [],
   providers: [],
