33619f69544fc2b44642538b97237ef654bb951b
JuanDuran85 <duran85juan@gmail.com>
2025-12-20 23:31:55 -0300

    feat: add PostgreSQL configuration and update main application entry point

--- teslo-shop/src/main.ts
+++ teslo-shop/src/main.ts
@@ -1,8 +1,9 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function main() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication<any> = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 main();
