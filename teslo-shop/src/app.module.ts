cc5c4d704112b776ef8edef93974aba8dfd72411
JuanDuran85 <duran85juan@gmail.com>
2025-12-20 23:51:29 -0300

    Refactor code structure for improved readability and maintainability

--- teslo-shop/src/app.module.ts
+++ teslo-shop/src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: process.env.DB_SYNC === 'true',
+      logging: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
