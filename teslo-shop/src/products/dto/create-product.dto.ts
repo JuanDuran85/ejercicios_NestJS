ccc452c9d09e54182e5715e8f134133d9cf50a05
JuanDuran85 <duran85juan@gmail.com>
2025-12-29 11:01:18 -0300

    feat(swagger): add OpenAPI documentation to DTOs
    Add @ApiProperty decorators to PaginationDto and CreateProductDto
    to enhance API documentation with descriptions and defaults.
    Also fix import path for PartialType in UpdateProductDto.
    
    BREAKING CHANGE: The UpdateProductDto now imports PartialType
    from @nestjs/swagger instead of @nestjs/mapped-types

--- teslo-shop/src/products/dto/create-product.dto.ts
+++ teslo-shop/src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsIn,
@@ -10,42 +11,78 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product title',
+    nullable: false,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty({
+    description: 'Product price',
+    default: 0,
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty({
+    description: 'Product description',
+    default: null,
+    nullable: true,
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({
+    description: 'Product slug',
+    nullable: true,
+    default: null,
+  })
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty({
+    description: 'Product stock',
+  })
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty({
+    description: 'Product sizes',
+    nullable: false,
+    minLength: 1,
+  })
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty({
+    description: 'Product gender',
+  })
   @IsIn(['men', 'women', 'kid', 'unisex'])
   @IsString()
   @IsOptional()
   gender?: string;
 
+  @ApiProperty({
+    description: 'Product tags',
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
 
+  @ApiProperty({
+    description: 'Product Image',
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
