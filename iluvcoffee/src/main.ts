09b70c487542b97c80c121a23e6a4a826a834140
JuanDuran85 <duran85juan@gmail.com>
2026-01-09 19:14:42 -0300

    Add global response wrapper interceptor

--- iluvcoffee/src/main.ts
+++ iluvcoffee/src/main.ts
@@ -2,6 +2,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters';
+import { WrapResponseInterceptor } from './common/interceptors';
 
 async function main() {
   const app: INestApplication = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function main() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 main();
