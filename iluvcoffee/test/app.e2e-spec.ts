0d2064dfb5a423905992e7306c40588657ed7237
JuanDuran85 <duran85juan@gmail.com>
2026-01-12 17:54:55 -0300

    Update Docker Compose commands and add e2e test for coffee creation

--- iluvcoffee/test/app.e2e-spec.ts
+++ iluvcoffee/test/app.e2e-spec.ts
@@ -1,12 +1,23 @@
-import { INestApplication } from '@nestjs/common';
+import {
+  HttpServer,
+  HttpStatus,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import request from 'supertest';
 import { App } from 'supertest/types';
+import { CreateCoffeeDto } from '../src/coffees/dto/create-coffee.dto';
 import { AppModule } from './../src/app.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
+  const coffee = {
+    name: 'Shipwreck Roast',
+    brand: 'Buddy Brew',
+    flavors: ['chocolate', 'vanilla'],
+  };
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -14,18 +25,26 @@ describe('AppController (e2e)', () => {
         AppModule,
         TypeOrmModule.forRoot({
           type: 'postgres',
-          host: 'localhost',
+          host: process.env.DB_HOST,
           port: 5433,
           username: process.env.DB_USERNAME,
           password: process.env.DB_PASSWORD,
           database: process.env.DB_NAME,
           autoLoadEntities: true,
-          synchronize: true,
+          synchronize: process.env.DB_SYNC === 'true',
         }),
       ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: { enableImplicitConversion: true },
+      }),
+    );
     await app.init();
   });
 
@@ -37,11 +56,13 @@ describe('AppController (e2e)', () => {
       .expect('Hello World!');
   });
 
-  it.todo('Create [Post /]');
-  it.todo('Get all [Get /]');
-  it.todo('Get one [Get /:id]');
-  it.todo('Update one [Patch /:id]');
-  it.todo('Delete one [Delete /:id]');
+  it('Create [Post /]', () => {
+    return request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .set('Authorization', process.env.API_KEY!)
+      .expect(HttpStatus.CREATED);
+  });
 
   afterAll(async () => {
     await app.close();
