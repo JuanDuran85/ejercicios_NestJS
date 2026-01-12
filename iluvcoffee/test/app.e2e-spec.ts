36b655ea9faff03016413c0e889c434ac5689c15
JuanDuran85 <duran85juan@gmail.com>
2026-01-12 17:35:43 -0300

    Add Docker test database and configure e2e testing with PostgreSQL

--- iluvcoffee/test/app.e2e-spec.ts
+++ iluvcoffee/test/app.e2e-spec.ts
@@ -1,15 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5433,
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -19,7 +32,18 @@ describe('AppController (e2e)', () => {
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
+      .set('Authorization', process.env.API_KEY!)
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it.todo('Create [Post /]');
+  it.todo('Get all [Get /]');
+  it.todo('Get one [Get /:id]');
+  it.todo('Update one [Patch /:id]');
+  it.todo('Delete one [Delete /:id]');
+
+  afterAll(async () => {
+    await app.close();
   });
 });
