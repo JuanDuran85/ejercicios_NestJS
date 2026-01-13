2f825e4b3b18b68dce57c31633e7031a263e6da5
JuanDuran85 <duran85juan@gmail.com>
2026-01-13 18:54:33 -0300

    Add basic Todo module with CRUD operations

---
+++ todo_basic/src/todo/todo.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
+
+@Injectable()
+export class TodoService {
+  private todos: Todo[] = [
+    {
+      id: 1,
+      title: 'Todo 1',
+      description: 'Description 1',
+      done: false,
+    },
+    {
+      id: 2,
+      title: 'Todo 2',
+      description: 'Description 2',
+      done: false,
+    },
+    {
+      id: 3,
+      title: 'Todo 3',
+      description: 'Description 3',
+      done: true,
+    },
+  ];
+
+  public create(createTodoDto: CreateTodoDto) {
+    return 'This action adds a new todo';
+  }
+
+  public findAll(): Todo[] {
+    return this.todos;
+  }
+
+  public findOne(id: number) {
+    return `This action returns a #${id} todo`;
+  }
+
+  public update(id: number, updateTodoDto: UpdateTodoDto) {
+    return `This action updates a #${id} todo`;
+  }
+
+  public remove(id: number) {
+    return `This action removes a #${id} todo`;
+  }
+}
