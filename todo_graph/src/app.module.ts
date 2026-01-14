f291ea099cffd8d669102fa7349b6fa9f948242c
JuanDuran85 <duran85juan@gmail.com>
2026-01-14 17:03:32 -0300

    feat: add GraphQL resolver with auto-schema generation and Apollo landing page

--- todo_graph/src/app.module.ts
+++ todo_graph/src/app.module.ts
@@ -1,12 +1,22 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'node:path';
+import { FirstResolveModule } from './first-resolve/first-resolve.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: false,
+      debug: true,
+      graphiql: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
     }),
+    FirstResolveModule,
   ],
   controllers: [],
   providers: [],
