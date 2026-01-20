903c62ace513281e39180bf0b68b8b8b750fdbfe
JuanDuran85 <duran85juan@gmail.com>
2026-01-20 18:40:29 -0300

    Add TypeORM and PostgreSQL integration to amylist module

--- amylist/src/app.module.ts
+++ amylist/src/app.module.ts
@@ -5,17 +5,34 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'node:path';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
-    driver: ApolloDriver,
-    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    sortSchema: true,
-    playground: false,
-    debug: true,
-    graphiql: false,
-    plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
-  }), ItemsModule, ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: false,
+      debug: true,
+      graphiql: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT! || 5432,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+      logger: 'debug'
+    }),
+    ItemsModule
+  ],
   controllers: [],
   providers: [],
 })
