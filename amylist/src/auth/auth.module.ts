9c35a2da86ffcdf2a9a48f8f6a90f1e53f851ab0
JuanDuran85 <duran85juan@gmail.com>
2026-01-23 19:12:13 -0300

    feat(auth): enhance AuthModule with JWT and Passport integration

--- amylist/src/auth/auth.module.ts
+++ amylist/src/auth/auth.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { CommonModule } from '../common/common.module';
 import { UsersModule } from '../users/users.module';
 import { AuthResolver } from './auth.resolver';
@@ -6,7 +9,22 @@ import { AuthService } from './auth.service';
 
 @Module({
   providers: [AuthResolver, AuthService],
-  imports: [UsersModule, CommonModule],
+  imports: [
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '5h',
+        },
+      }),
+    }),
+    CommonModule,
+    UsersModule,
+  ],
   controllers: [],
   exports: [AuthService],
 })
