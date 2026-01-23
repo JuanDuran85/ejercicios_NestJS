9de7821b1112d7f401d689b9aaf0655dcc6d18bb
JuanDuran85 <duran85juan@gmail.com>
2026-01-23 12:33:21 -0300

    feat(users): add user entity with roles and status fields; update module and schema for user management

--- amylist/src/users/users.module.ts
+++ amylist/src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 import { UsersResolver } from './users.resolver';
+import { UsersService } from './users.service';
 
 @Module({
   providers: [UsersResolver, UsersService],
+  exports: [UsersService],
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [],
 })
-export class UsersModule {}
+export class UsersModule { }
