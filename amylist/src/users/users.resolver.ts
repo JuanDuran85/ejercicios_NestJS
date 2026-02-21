26244be7c50bd5101ea7eaa4b4f950f58455097f
JuanDuran85 <duran85juan@gmail.com>
2026-02-21 18:35:01 -0300

    Add roles filtering parameter to users query

--- amylist/src/users/users.resolver.ts
+++ amylist/src/users/users.resolver.ts
@@ -1,24 +1,31 @@
-import { Args, ID, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { UpdateUserInput } from './dto/update-user.input';
+import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { ValidRolesArgs } from './dto/args/roles.arg';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 @Resolver(() => User)
 export class UsersResolver {
-  constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
-  public async findAll(): Promise<User[]> {
+  public async findAll(
+    @Args() validRoles: ValidRolesArgs,
+  ): Promise<User[]> {
+    console.debug({ validRoles });
     return this.usersService.findAll();
   }
 
   @Query(() => User, { name: 'user' })
-  public async findOne(@Args('id', { type: () => ID }) id: string): Promise<User> {
+  public async findOne(
+    @Args('id', { type: () => ID }) id: string,
+  ): Promise<User> {
     return this.usersService.findOne(id);
   }
 
   @Mutation(() => User)
-  public async blockUser(@Args('id', { type: () => ID }) id: string) : Promise<User> {
+  public async blockUser(
+    @Args('id', { type: () => ID }) id: string,
+  ): Promise<User> {
     return this.usersService.block(id);
   }
 }
