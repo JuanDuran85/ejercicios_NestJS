9de51ca91148978694199ef3723ab24a9168333b
JuanDuran85 <duran85juan@gmail.com>
2026-03-07 20:28:08 -0300

    refactor(basic-reports): improve PDF generation logic and remove unnecessary code

--- report-pdf/src/basic-reports/basic-reports.controller.ts
+++ report-pdf/src/basic-reports/basic-reports.controller.ts
@@ -1,30 +1,19 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import type { Response } from 'express';
 import { BasicReportsService } from './basic-reports.service';
+import type { TCreatedPdf } from 'pdfmake';
 
 @Controller('basic-reports')
 export class BasicReportsController {
   constructor(private readonly basicReportsService: BasicReportsService) {}
 
   @Get()
-  public getBasicReports(@Res() response: Response) {
-    const resultPdfCreated = this.basicReportsService.getBasicReport();
+  public async getBasicReports(@Res() response: Response) {
+    const resultPdfCreated: TCreatedPdf =
+      this.basicReportsService.getBasicReport();
 
     response.setHeader('Content-Type', 'application/pdf');
-    resultPdfCreated
-      .write('result.pdf')
-      .then(
-        () => {
-          console.debug(new Date());
-        },
-        (err) => {
-          console.debug(err);
-        },
-      )
-      .catch((err) => console.debug(err))
-      .finally(() => {
-        console.debug('finishing');
-      });
-     return response.send(resultPdfCreated);
+    const result: Buffer<ArrayBufferLike> = await resultPdfCreated.getBuffer();
+    return response.send(result);
   }
 }
