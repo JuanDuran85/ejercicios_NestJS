60d47dbb0e38448035e6e88137b2bc12b18af060
JuanDuran85 <duran85juan@gmail.com>
2026-03-16 10:08:10 -0300

    feat: add HTML report generation with basic structure and dependencies

--- report-pdf/src/extra-report/extra-report.service.ts
+++ report-pdf/src/extra-report/extra-report.service.ts
@@ -3,6 +3,7 @@ import type { TCreatedPdf } from 'pdfmake';
 import { PrinterService } from '../printer/printer.service';
 import { PrismaService } from '../prisma.service';
 import { getFinalBasicReport } from '../reports';
+import fs from 'node:fs';
 
 @Injectable()
 export class ExtraReportService {
@@ -11,8 +12,10 @@ export class ExtraReportService {
     private readonly printerService: PrinterService,
   ) {}
   public getHtmlReport(): TCreatedPdf {
+    const htmlFile: string = fs.readFileSync('src/reports/html/basic-01.html', 'utf8');
+    console.debug(htmlFile);
     const docDefinition = getFinalBasicReport({ name: 'John Doe' });
-    
+
     return this.printerService.createPdf(docDefinition, {
       autoPrint: true,
       bufferPages: true,
