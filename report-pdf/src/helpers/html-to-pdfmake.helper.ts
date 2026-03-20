1082108751c75505633ed387516dbf8ac712aed5
Juan Duran <duran85juan@gmail.com>
2026-03-20 19:29:34 +0000

    add jsdo

--- report-pdf/src/helpers/html-to-pdfmake.helper.ts
+++ report-pdf/src/helpers/html-to-pdfmake.helper.ts
@@ -1,6 +1,7 @@
 import htmlToPdfmake from 'html-to-pdfmake';
 import type { Content } from 'pdfmake';
-
+import {JSDOM} from 'jsdom';
 export const getHtmlContent: (htmlIn: string) => Content = (htmlIn: string) => {
-  return htmlToPdfmake(htmlIn);
+  const {window} = new JSDOM();
+  return htmlToPdfmake(window);
 };
