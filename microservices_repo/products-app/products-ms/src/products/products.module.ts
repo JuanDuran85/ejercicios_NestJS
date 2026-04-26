bf6ef8b8c6248645b1ae1ef4efe2478d2092787d
JuanDuran85 <duran85juan@gmail.com>
2026-04-26 11:44:55 -0400

    refactor: reorganize imports and add explicit access modifiers to products module
    - Alphabetize imports in products controller and module
    - Add 'public' access modifier to all controller methods
    - Add empty imports and exports arrays to products module decorator

--- microservices_repo/products-app/products-ms/src/products/products.module.ts
+++ microservices_repo/products-app/products-ms/src/products/products.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
+  imports: [],
+  exports: []
 })
 export class ProductsModule {}
