f582898dda39ce5c8373679a1141baad6e9f8b83
JuanDuran85 <duran85juan@gmail.com>
2026-05-17 19:41:35 -0400

    refactor(auth-ms): remove boilerplate app controller and service

--- microservices_repo/products-app/auth-ms/src/main.ts
+++ microservices_repo/products-app/auth-ms/src/main.ts
@@ -1,6 +1,6 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const app: INestApplication<any> = await NestFactory.create(AppModule);
