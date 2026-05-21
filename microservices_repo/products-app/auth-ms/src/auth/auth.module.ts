eab11c6ab3feb23e470ca5e75effbef5b247fbba
JuanDuran85 <duran85juan@gmail.com>
2026-05-20 22:28:33 -0400

    feat(auth-ms): remove old .eslintignore files and add new eslint.config.js for multiple services; update package.json and package-lock.json with NATS and ESLint dependencies

--- microservices_repo/products-app/auth-ms/src/auth/auth.module.ts
+++ microservices_repo/products-app/auth-ms/src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { NatsModule } from '../transports/nats.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [NatsModule],
+  exports: [],
 })
 export class AuthModule {}
