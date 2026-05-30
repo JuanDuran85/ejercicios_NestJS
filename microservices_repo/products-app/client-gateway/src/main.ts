edc6ff8357515e1f4d9fee9533783f679a5a46ca
JuanDuran85 <duran85juan@gmail.com>
2026-05-30 17:08:42 -0400

    feat(health-check): add HealthCheck module and controller for service health monitoring

--- microservices_repo/products-app/client-gateway/src/main.ts
+++ microservices_repo/products-app/client-gateway/src/main.ts
@@ -1,4 +1,9 @@
-import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+import {
+  INestApplication,
+  Logger,
+  RequestMethod,
+  ValidationPipe,
+} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RpcCustomExceptionFilter } from './common';
@@ -8,7 +13,14 @@ async function bootstrap() {
   const { port } = envs;
   const logger: Logger = new Logger('Main-Gateway');
   const app: INestApplication<any> = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: [
+      {
+        path: '',
+        method: RequestMethod.GET,
+      },
+    ],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
