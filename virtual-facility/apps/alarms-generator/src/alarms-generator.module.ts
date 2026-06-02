44f680216f34fe90d35f857bd32bcd3a63656e40
JuanDuran85 <duran85juan@gmail.com>
2026-06-02 11:37:11 -0400

    feat(alarms-generator): configure NATS connection with environment variable
    Add ConfigModule and configure NATS client to use NATS_URL from environment variables instead of default connection settings

--- virtual-facility/apps/alarms-generator/src/alarms-generator.module.ts
+++ virtual-facility/apps/alarms-generator/src/alarms-generator.module.ts
@@ -1,16 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AlarmsGeneratorService } from './alarms-generator.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ALARMS_SERVICE } from './constants';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), ClientsModule.register([
-    {
-      name: ALARMS_SERVICE,
-      transport: Transport.NATS
-    }
-  ])],
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot(),
+    ClientsModule.register([
+      {
+        name: ALARMS_SERVICE,
+        transport: Transport.NATS,
+        options: {
+          servers: process.env.NATS_URL,
+        },
+      },
+    ]),
+  ],
   controllers: [],
   providers: [AlarmsGeneratorService],
 })
